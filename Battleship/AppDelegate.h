5bdad0a44312ba9235e7c378e9b0e9bae5d900ec
Ahmet Geymen <ahmet@valensas.com>
2013-04-20 03:00:37 +0300

    Initial Commit

---
+++ Battleship/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Battleship
+//
+//  Created by Ahmet Geymen on 4/20/13.
+//  Copyright (c) 2013 Ahmet Geymen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Battleship/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Battleship
+//
+//  Created by Ahmet Geymen on 4/20/13.
+//  Copyright (c) 2013 Ahmet Geymen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
